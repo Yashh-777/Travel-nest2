f4516e10a9f9418192e277e0e7c2f8c1ab7a1e5f
sharmaya_1 <112720965+Yashh-777@users.noreply.github.com>
2023-07-10 13:08:03 +0530

    Update room.js

--- controllers/room.js
+++ controllers/room.js
@@ -52,7 +52,22 @@ export const updateRoomAvailability = async (req, res, next) => {
 };
 
 
-
+export const deleteRoom = async (req, res, next) => {
+  const hotelId = req.params.hotelid;
+  try {
+    await Room.findByIdAndDelete(req.params.id);
+    try {
+      await Hotel.findByIdAndUpdate(hotelId, {
+        $pull: { rooms: req.params.id },
+      });
+    } catch (err) { 
+      next(err);
+    }
+    res.status(200).json("Room has been deleted.");
+  } catch (err) {
+    next(err);
+  }
+};
 
 
 export const getRoom = async (req, res, next) => {
